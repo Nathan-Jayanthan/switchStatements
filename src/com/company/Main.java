24a55ad3848b339c64ff5d4a663180034e97a702
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-25 18:46:29 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age;
+        age = 3;
+
+        switch (age){
+            case 1:
+                System.out.println("crawl");
+                break;
+            case 2:
+                System.out.println("talk");
+                break;
+                //if none of the other cases match the value go here
+            default:
+                System.out.println("DONT KNOW AGE");
+                break;
+        }
+
+    }
+}
